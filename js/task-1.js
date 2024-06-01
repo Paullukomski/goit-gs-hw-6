34b8dbcf0660ccdadbb79d2c65c570af3806b214
Paullukomskyi <paulolegovych@gmail.com>
2024-06-01 15:36:35 +0200

    main

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,276 @@
-const name = ["hello", "js"];
-console.log(name);
+// "use strict";
+// const name = ["hello", "js"];
+// console.log(name);
+// function sayHello(greeting) {
+//   console.log(`${greeting}, ${this.name}!`);
+// }
+
+// const user = {
+//   name: "Alice",
+// };
+
+// const greet = sayHello.bind(user);
+
+// greet("Hello"); //Hello Alice
+// "use strict";
+
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   // callback() — це виклик методу getFullName в глобальному контексті
+//   const username = callback();
+//   console.log(`Processing an application from ${username}`);
+// }
+
+// makeMessage(customer.getFullName); // TypeError: Cannot read properties of undefined (reading 'firstName')
+
+// const customer = {
+//   firstName: "Jacob",
+//   lastName: "Mercer",
+//   getFullName() {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// function makeMessage(callback) {
+//   const username = callback();
+//   console.log(`Processing an application from ${username}`);
+// }
+
+// makeMessage(customer.getFullName.bind(customer)); // "Processing an application from Jacob Mercer"
+// const library = {
+//   books: 1923,
+//   logBookCount() {
+//     console.log(this.books);
+//   },
+// };
+
+// const showBooks = library.logBookCount.bind({ books: 724 });
+
+// showBooks();
+// ("use strict");
+
+// const library = {
+//   books: 1923,
+//   logBookCount() {
+//     console.log(this.books);
+//   },
+// };
+
+// function showBooks(callback) {
+//   callback();
+// }
+
+// showBooks(library.logBookCount);
+// library.logBookCount.bind(library);
+// showBooks(logBookCount.bind(library));
+// showBooks(library.logBookCount.bind(library)); // OK
+
+// "use strict";
+
+// const showThis = () => {
+//   console.log("this in showThis: ", this);
+// };
+
+// showThis(); // this in showThis: window
+// "use strict";
+
+// const showThis = () => {
+//   console.log("this in showThis: ", this);
+// };
+
+// showThis(); // this in showThis: window
+//***** */
+// const customer = {
+//   username: "Jacob",
+// };
+// const animal = {
+//   legs: 4,
+// };
+// const dog = Object.create(animal);
+// dog.name = "mango";
+// console.log(dog); //  {name; "Mango", [[Prototype]]: animal}
+// console.log(animal.isPrototypeOf(dog));
+// console.log(dog.isPrototypeOf(animal)); // falce
+// console.log(customer.isPrototypeOf(dog)); //  falce
+// //
+// //
+// console.log(dog.hasOwnProperty("name")); //true
+// console.log(dog.hasOwnProperty("legs")); //falce
+
+// const apartment = {
+//   rooms: 4,
+//   floor: 2,
+//   taxFree: falce,
+// };
+// const condo = Object.create(apartment);
+// condo.rooms = 3;
+
+// class Car {
+//   #brand;
+//   constructor(params) {
+//     this.#brand = params.brand;
+//     this.model = params.model;
+//     this.price = params.price;
+//   }
+
+//   getPrice() {
+//     return this.#brand;
+//   }
+
+//   changePrice(newBrand) {
+//     this.#brand = newBrand;
+//   }
+// }
+// class Car {
+//   static #maxPrice = 50000;
+
+//   constructor(params) {
+//     this.price = params.price;
+//   }
+//   checkPrice(price) {
+//     if (price <= maxPrice) {
+//       return "Success! Price is within acceptable limits";
+//     }
+//     return "Error! Price exceeds the maximum";
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// class Car {
+//   static #maxPrice = 50000;
+//   #price;
+//   constructor(params) {
+//     this.price = params.price;
+//   }
+//   get price() {
+//     return this.#price;
+//   }
+//   set price(newPrice) {
+//     if (newPrice <= Car.#maxPrice) {
+//       this.#price = newPrice;
+//     }
+//   }
+//   static checkPrice(price) {
+//     if (price > Car.#maxPrice) {
+//       return "Error! Price exceeds the maximum";
+//     } else {
+//       return "Success! Price is within acceptable limits";
+//     }
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+
+// console.log(Car.checkPrice(audi.price)); // "Success! Price is within acceptable limits"
+// console.log(Car.checkPrice(bmw.price)); // "Error! Price exceeds the maximum"
+
+// class User {
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   static role = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+// }
+
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   static role = {
+//     BASIC: "basic",
+//     SUPERUSER: "superuser",
+//   };
+
+//   constructor(params) {
+//     super(params.email);
+//     this.access = params.access;
+//     this.blacklistedEmails = [];
+//   }
+//   blacklist(email) {
+//     this.blacklistedEmails.push(email);
+//   }
+//   isBlacklisted(email) {
+//     return this.blacklistedEmails.includes(email);
+//   }
+// }
+
+// const mango = new Admin({
+//   email: "mango@mail.com",
+//   access: Admin.role.SUPERUSER,
+// });
+
+// console.log(mango.email); // "mango@mail.com"
+// console.log(mango.access); // "superuser"
+
+// mango.blacklist("poly@mail.com");
+// console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+// console.log(mango.isBlacklisted("mango@mail.com")); // false
+// console.log(mango.isBlacklisted("poly@mail.com")); // true
+
+const customer = {
+  username: "Mango",
+  balance: 24000,
+  discount: 0.1,
+  orders: ["Burger", "Pizza", "Salad"],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
+  getDiscount() {
+    return this.discount;
+  },
+  setDiscount(value) {
+    discount = value;
+  },
+  getOrders() {
+    return this.orders;
+  },
+  addOrder(cost, order) {
+    this.balance -= cost - cost * discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
+};
+
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
